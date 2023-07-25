79af3dac8f80356461738094c306a069b12dc7cb
Alexander Romanenko <alex@alex-tech-adventures.com>
2023-07-26 00:23:12 +0300

    Упростить страницу списка для подготовки таблицы и словаря.

---
+++ BC_FeatureTracker/Ata.DeloSled.FeatureTracker.Domain/WorkItem.cs
@@ -0,0 +1,51 @@
+﻿using SharedKernel;
+
+namespace Ata.DeloSled.FeatureTracker.Domain;
+
+public class WorkItem : Entity
+{
+    public WorkItem(string title)
+    {
+        Title = title;
+    }
+
+    public StateEnum StateId { get; init; } // TODO сделать private set
+    public IWorkItemState State { get; } = new NewState();
+    public string Title { get; init; }
+}
+
+public enum StateEnum
+{
+    New,
+    Active,
+    Closed,
+}
+
+
+public abstract class Lookup
+{
+    public abstract string DisplayName { get; }
+}
+
+public interface IWorkItemState
+{
+    public abstract string DisplayName { get; }
+}
+
+public class NewState : Lookup, IWorkItemState
+{
+    public StateEnum Id => StateEnum.New;
+    public override string DisplayName => "New";
+}
+
+public class ActiveState : Lookup,  IWorkItemState
+{
+    public StateEnum Id => StateEnum.Active;
+    public override string DisplayName => "Active";
+}
+
+public class ClosedState : Lookup, IWorkItemState
+{
+    public StateEnum Id => StateEnum.Closed;
+    public override string DisplayName => "Closed";
+}
--- SharedKernel/Entity.cs
+++ SharedKernel/Entity.cs
@@ -7,6 +7,8 @@ namespace SharedKernel
     {
         [JsonProperty]
         public Guid Guid { get; protected set; } = Guid.NewGuid();
+
+        public bool IsActive { get; init; }
         public override bool Equals(object obj)
         {
             return (obj as IEntity)?.Guid == Guid;
